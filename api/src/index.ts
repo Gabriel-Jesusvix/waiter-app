b8d383db0deaa3f765fd275d4719da6785988d58
Gabriel Jesus <gabrieldejesus16gs@gmail.com>
2024-11-27 15:44:09 -0300

    chore: config mongoose to connection db

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,7 +1,15 @@
-import express from 'express'
+import express from "express";
+import mongoose from "mongoose";
 
-const app = express()
-const PORT = 3001
-app.listen(PORT, () => {
-  console.log(`🚀 Server is running on http://localhost:${PORT}`)
-})
+mongoose
+  .connect("mongodb://localhost:27017")
+  .then(() => {
+    const app = express();
+    const PORT = 3001;
+    app.listen(PORT, () => {
+      console.log(`🚀 Server is running on http://localhost:${PORT}`);
+    });
+  })
+  .catch(() => {
+    console.log("error: connection refused");
+  });
