5b0d7ceec4d9bf0b53b4de3f25dbf79c0d93ab6f
Gabriel Jesus <gabrieldejesus16gs@gmail.com>
2024-11-29 16:58:56 -0300

    feat: implements useCase List Order

--- api/src/app/router.ts
+++ api/src/app/router.ts
@@ -7,6 +7,7 @@ import { listProducts } from "./useCases/products/listProducts";
 import multer from 'multer'
 import { createProduct } from "./useCases/products/createProduct";
 import { listProductsByCategory } from "./useCases/categories/listProductsByCategory";
+import { listOrders } from "./useCases/orders/listOrdes";
 
 
 export const router = Router();
@@ -33,9 +34,7 @@ router.post('/products', upload.single('image'), createProduct);
 
 router.get("/categories/:categoryId/products",listProductsByCategory);
 
-router.get("/orders", (request, response) => {
-  response.send("OK");
-});
+router.get("/orders", listOrders);
 
 router.post("/orders", (request, response) => {
   response.send("OK");
