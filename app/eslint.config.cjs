255d85e8ffb0f1645a56ec53b0c69506cdacfa69
Gabriel Jesus <gabrieldejesus16gs@gmail.com>
2024-12-03 15:23:48 -0300

    feat: create structure initial home

--- app/eslint.config.cjs
+++ app/eslint.config.cjs
@@ -10,7 +10,7 @@ module.exports = [
                 ecmaVersion: "latest",
                 sourceType: "module",
                 ecmaFeatures: {
-                    jsx: true,
+                    jsx: false,
                 },
             },
         },
---
+++ app/src/Main/index.tsx
@@ -0,0 +1,20 @@
+import { Categories } from "../components/Categories";
+import { Header } from "../components/Header";
+import { Menu } from "../components/Menu";
+import { Text } from "../components/Text";
+import { CategoriesContainer, Container, MenuContainer } from "./styles";
+
+
+export function Main (){
+  return (
+    <Container>
+      <Header />
+      <CategoriesContainer>
+        <Categories />
+      </CategoriesContainer>
+      <MenuContainer>
+        <Menu />
+      </MenuContainer>
+    </Container>
+  )
+}
