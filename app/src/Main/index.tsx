fdb3319e2868486b52106aa3f2a40faffdf6a9ca
Gabriel Jesus <gabrieldejesus16gs@gmail.com>
2024-12-04 17:16:03 -0300

    feat: create component tableModal

--- app/src/Main/index.tsx
+++ app/src/Main/index.tsx
@@ -1,35 +1,47 @@
+import { useState } from "react";
 import { Button } from "../components/Button";
 import { Categories } from "../components/Categories";
 import { Header } from "../components/Header";
 import { Menu } from "../components/Menu";
+import { TableModal } from "../components/TableModal";
 import { products } from "../mocks/products";
 import { CategoriesContainer, Container, Footer, FooterContainer, MenuContainer } from "./styles";
 
 
 export function Main (){
+	const [isTableModalVisible, setIsTableModalVisible] = useState(false);
+
   return (
-    <Container>
-      <Header />
-      <CategoriesContainer>
-        <Categories />
-      </CategoriesContainer>
-      <MenuContainer>
-        <Menu
-          onAddToCart={() =>{}}
-          products={products}
-        />
-      </MenuContainer>
-      <Footer>
-        <FooterContainer>
-          <Button
-            loading={false}
-            onPress={()=>{}}
-            disabled={false}
-          >
-            Novo pedido
-          </Button>
-        </FooterContainer>
-      </Footer>
-    </Container>
+    <>
+      <Container>
+        <Header />
+        <CategoriesContainer>
+          <Categories />
+        </CategoriesContainer>
+        <MenuContainer>
+          <Menu
+            onAddToCart={() =>{}}
+            products={products}
+          />
+        </MenuContainer>
+        <Footer>
+          <FooterContainer>
+            <Button
+              loading={false}
+              onPress={() => setIsTableModalVisible(true)}
+              disabled={false}
+            >
+              Novo pedido
+            </Button>
+          </FooterContainer>
+        </Footer>
+      </Container>
+      <TableModal
+        visible={isTableModalVisible}
+        onClose={() => setIsTableModalVisible(false)}
+        onSave={() =>{}}
+      />
+    </>
+
   )
 }
