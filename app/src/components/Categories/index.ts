e583dfaa9b7b073209b1d74d7b59bd823b1b2096
Gabriel Jesus <gabrieldejesus16gs@gmail.com>
2024-12-04 14:36:42 -0300

    feat: create menu categories and button component

--- app/src/components/Categories/index.ts
+++
@@ -1,5 +0,0 @@
-
-
-export function Categories() {
-  return null
-}
---
+++ app/src/components/Categories/index.tsx
@@ -0,0 +1,42 @@
+import { FlatList } from "react-native"
+import { categories } from "../../mocks/categories"
+import { CategoryContainer, Icon } from "./styles";
+import { Text } from "../Text";
+import { useState } from "react";
+
+
+export function Categories() {
+	const [selectedCategory, setSelectedCategory ] = useState('');
+
+  function handleSelectCategory(categoryId: string) {
+		const category = selectedCategory === categoryId ? '' : categoryId;
+
+		// onSelectCategory(category);
+		setSelectedCategory(category);
+	}
+
+  return (
+    <FlatList
+			horizontal
+			showsHorizontalScrollIndicator={false}
+			data={categories}
+			contentContainerStyle={{ paddingRight: 24}}
+			keyExtractor={category => category._id}
+			renderItem={({ item: category }) => {
+				const isSelected = selectedCategory === category._id;
+				return (
+					<CategoryContainer onPress={() => handleSelectCategory(category._id)}>
+						<Icon>
+							<Text opacity={isSelected ? 1 : 0.5}>
+								{category.icon}
+							</Text>
+						</Icon>
+						<Text size={14} weight='600' opacity={isSelected ? 1 : 0.5}>
+							{category.name}
+						</Text>
+					</CategoryContainer>
+				);
+			}}
+		/>
+  )
+}
